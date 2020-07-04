af37c9a1ed61b25d38ce619eb128d9a5efa04ff9
Leslie Hoare <iam@lesleh.co.uk>
2020-07-04 22:27:38 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { jsx } from "@emotion/core";
+import styled from "@emotion/styled";
+
+const Title = styled.h1`
+  color: red;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title>This is shielded from global styles</Title>
     </div>
   );
 }
